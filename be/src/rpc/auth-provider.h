f101a7f71fdede272d756218642c3230895ae01c
Sailesh Mukil <sailesh@cloudera.com>
2017-03-28 15:56:26 -0700

    IMPALA-5129: Use KRPC's Kinit code to avoid expensive fork
    Impala currently kinits by forking off a child process. This
    has proved to be expensive in many cases since the subprocess
    tries to reserve as much memory as Impala is currently using
    which can be quite a lot.
    
    This patch adds a flag called 'use_krpc_kinit' that defaults to
    true. When it's true, it uses the Kudu security library's kinit code
    that programatically uses the krb5 library to kinit.
    When it's false, we run our current path which kicks off the
    kinit-thread and forks off a kinit process periodically to reacquire
    tickets based on FLAGS_kerberos_reinit_interval.
    
    Converted existing tests in thrift-server-test to run with and
    without kerberos. We now run this BE test with kerberos by using
    Kudu's MiniKdc utility. This introduces a new dependency on some
    kerberos binaries that are checked through FindKerberosPrograms.cmake.
    Note that this is only a test dependency and not a dependency for
    the impalad binaries and friends. Compilation will still succeed if
    the kerberos binaries for the MiniKdc are not found, however, the
    thrift-server-test will fail.
    
    Updated bin/bootstap_development.sh to install new sasl-gssapi
    modules and the kerberos binaries required for the MiniKdc.
    
    Testing: Verified with thrift-server-test and also manually on a
    live kerberized cluster.
    
    Change-Id: I9cea56cc6e7412d87f4c2e92399a2f91ea6af6c7

--- be/src/rpc/auth-provider.h
+++ be/src/rpc/auth-provider.h
@@ -140,7 +140,7 @@ class SaslAuthProvider : public AuthProvider {
   /// function as a client.
   bool needs_kinit_;
 
-  /// Runs "RunKinit" below if needs_kinit_ is true.
+  /// Runs "RunKinit" below if needs_kinit_ is true and FLAGS_use_krpc_kinit is false.
   std::unique_ptr<Thread> kinit_thread_;
 
   /// Periodically (roughly once every FLAGS_kerberos_reinit_interval minutes) calls kinit
